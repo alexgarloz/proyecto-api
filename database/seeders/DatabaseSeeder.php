15a31fc9da96716b31e2bbd00c6559eb26cdd1fd
alexgarloza@gmail.com <alexgarloza@gmail.com>
2022-02-24 19:50:53 +0100

    declarados los nuevos Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(1000)->create();
-        \App\Models\Tipo::factory(1000)->create();
+        \App\Models\Tipo::factory(10)->create();
         \App\Models\Direccion::factory(1000)->create();
-        \App\Models\Categoria::factory(1000)->create();
-        \App\Models\SubCategoria::factory(1000)->create();
-        \App\Models\Servicio::factory(1000)->create();
+        \App\Models\Categoria::factory(30)->create();
+        \App\Models\SubCategoria::factory(50)->create();
+        \App\Models\Servicio::factory(300)->create();
         \App\Models\Comentario::factory(1000)->create();
-        \App\Models\ContratoServicio::factory(1000)->create();
+        \App\Models\ContratoServicio::factory(280)->create();
     }
 }
