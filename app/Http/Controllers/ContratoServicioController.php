18787cfe11555f809a5d9d330dead303d95bbf64
alexgarloza@gmail.com <alexgarloza@gmail.com>
2022-03-04 18:53:12 +0100

    devolviendo los servicios contratados por usuario

--- app/Http/Controllers/ContratoServicioController.php
+++ app/Http/Controllers/ContratoServicioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ContratoServicio;
+use Carbon\Carbon;
 
 class ContratoServicioController extends Controller
 {
@@ -16,13 +17,31 @@ class ContratoServicioController extends Controller
                     'message' => 'Error al Obtener el Contrato Servicio',
                     'data' => null
                 ], 404);
-            } else {
-                return response()->json([
-                    'success' => true,
-                    'message' => 'Contrato Servicio Obtenidos Correctamente',
-                    'data' => $idContratoServicio
-                ]);
             }
+            $resultadoAnydados = [];
+            foreach ($idContratoServicio as $ficha_contrato) {
+                $resultadoAnydados[] = [
+                    'id' => $ficha_contrato->id,
+                    'descripcion' => $ficha_contrato->servicio->descripcion,
+                    'precio' => str_replace('.', ',', $ficha_contrato->precio),
+                    'fecha_inicio' => Carbon::parse($ficha_contrato->fecha_inicio)->locale('es')
+                        ->format('d M, Y H:m'),
+                    'fecha_fin' => Carbon::parse($ficha_contrato->fecha_fin)->locale('es')
+                        ->format('d M, Y H:m'),
+                    'servicio_nombre' => $ficha_contrato->servicio->nombre,
+                    'usuario_nombre' => $ficha_contrato->usuario->nombre,
+                    'fecha_status' => date('Ynj', strtotime($ficha_contrato->fecha_fin))
+                ];
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Contrato Servicio Obtenidos Correctamente',
+                'data' => $resultadoAnydados
+            ], 200);
+
+
+
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
